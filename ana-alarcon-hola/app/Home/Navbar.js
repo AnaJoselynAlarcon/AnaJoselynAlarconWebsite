80745f6dfbe18e8abdd1288c30e7ad7b4b68c1d3
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2023-11-22 12:50:21 -0700

    added image

---
+++ ana-alarcon-hola/app/Home/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 w-full flex items-center sticky top-0 px-30 pb-5 z-10 justify-between">
+      <span className="flex items-center pt-2 pr-6">Hola Welcome!</span>
+    </div>
+  );
+};
+
+export default Navbar;
